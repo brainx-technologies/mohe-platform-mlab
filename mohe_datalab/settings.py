eeb09592a1a5bf8c20fad4d5b4a2a092a4b99540
Christian Thieme <c.thieme@me.com>
2023-06-19 15:01:06 +0200

    settings

--- mohe_datalab/settings.py
+++ mohe_datalab/settings.py
@@ -2,6 +2,10 @@ from mohe.settings.base import *
 
 PROJECT_DIR = Path(__file__).resolve().parent
 
+ALLOWED_HOSTS = [
+    'mlab.' + config.get('MOHE', 'DOMAIN')
+]
+
 # Application definition
 
 INSTALLED_APPS = [
